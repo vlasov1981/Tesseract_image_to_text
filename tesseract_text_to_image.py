7f8ab5cfcc2cfd93a7aaba31942e91164dd41a95
vlasov1981@yahoo.com <77mj&)(13A>
2022-05-27 16:26:33 +0300

    Final commit

--- tesseract_text_to_image.py
+++ tesseract_text_to_image.py
@@ -1,1 +1,9 @@
-print("test")
+import pytesseract
+from PIL import Image
+
+file_name = "numbers"
+
+img = Image.open(f'C:\\Users\\vlaso\\OneDrive\\images_for_tesseract\\{file_name}.png')
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  # path to installed tesseract for Windows
+text = pytesseract.image_to_string(img)
+print(text)
